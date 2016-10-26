9b3bb35309cb0a411ca7e285c0fb4eb84ce37ce1
郑鑫祺 <candy.zhengxq@gmail.com>
2016-10-26 18:31:44 +0800

    add change pic by keyword support and add overlap judge

--- src/states/MainState.js
+++ src/states/MainState.js
@@ -28,7 +28,6 @@ export class MainState extends Phaser.State {
     this.game.time.events.loop(100, this.spawnFruits, this);
     this.scoreLabel = this.game.add.text(30, 30, 'score: 0',
     { font: '18px Arial', fill: '#ffffff' });
-    console.log(this.game.physics.arcade.overlap);
   }
   update() {
     this.game.physics.arcade.overlap(this.player, this.fruits, this.takeFruit, null, this);
@@ -38,11 +37,14 @@ export class MainState extends Phaser.State {
     const getKeyMapCondition = (key) => this.cursor[key].isDown;
     // 当向左运动时
     if (getKeyMapCondition('left')) {
+      // change the pic
+      this.player.loadTexture('banana');
       if (this.player.x > 25) {
         this.player.x -= 2;
       }
     // 当向右运动时
     } else if (getKeyMapCondition('right')) {
+      this.player.loadTexture('player');
       if (this.player.x < this.screenWidth - 25) {
         this.player.x += 2;
       }
@@ -54,15 +56,20 @@ export class MainState extends Phaser.State {
     const fruit = this.game.add.sprite(10 + Math.floor(Math.random() * 1000), 0, fruits[randomFruitIndex]);
     this.fruits.add(fruit);
     fruit.scale.setTo(0.02, 0.02);
+    fruit.anchor.setTo(0.5, 0.5);
     this.game.physics.arcade.enable(fruit);
     fruit.body.gravity.y = 500;
     fruit.checkWorldBounds = true;
     fruit.outOfBoundsKill = true;
   }
   takeFruit(player, fruit) {
-    fruit.kill();
-    this.score += 5;
-    this.scoreLabel.text = `score: ${this.score}`;
+    const playerPositon = player.body.position;
+    const fruitPosition = fruit.position;
+    if (fruitPosition.x > playerPositon.x + player.body.halfWidth) {
+      fruit.kill();
+      this.score += 5;
+      this.scoreLabel.text = `score: ${this.score}`;
+    }
   }
 }
 
