b8090cc63d63a01d09c9d122bfa646b479ba9580
郑鑫祺 <candy.zhengxq@gmail.com>
2016-10-26 23:05:31 +0800

    add drag support

--- src/states/PlayState.js
+++ src/states/PlayState.js
@@ -6,17 +6,18 @@ export class PlayState extends Phaser.State {
     this.difficult = 10;
     this.score = 0;
   }
-  preload() {
-    this.game.load.image('player', 'assets/fruit.png');
-    this.game.load.image('banana', 'assets/banana.png');
-    this.game.load.image('apple', 'assets/fruit.png');
-  }
   create() {
     this.cursor = this.game.input.keyboard.createCursorKeys();
+    this.pointer = this.game.input.addPointer();
     // create a player
     this.player = this.game.add.sprite(30, this.screenHeight - 100, 'player');
     this.player.anchor.setTo(0.5, 0.5);
     this.player.scale.setTo(0.05, 0.05);
+    // add drag event in item
+    this.player.inputEnabled = true;
+    this.player.input.enableDrag(true);
+    this.player.events.onDragUpdate.add(this.onDragFixY);
+
     this.game.physics.arcade.enable(this.player);
     this.fruits = this.game.add.group();
     this.fruits.enableBody = true;
@@ -26,9 +27,23 @@ export class PlayState extends Phaser.State {
   }
   update() {
     this.game.physics.arcade.overlap(this.player, this.fruits, this.takeFruit, null, this);
-    this.movePlayer();
+    this.movePlayerByKeys();
   }
-  movePlayer() {
+  render() {
+    // for debug
+    // this.game.debug.inputInfo(30, 240);
+  }
+  onDragFixY(item) {
+    if (item.previousPosition.y !== item.y) {
+      item.y = item.previousPosition.y;
+    }
+    if (item.previousPosition.x < item.x) {
+      item.loadTexture('player');
+    } else if (item.previousPosition.x > item.x) {
+      item.loadTexture('banana');
+    }
+  }
+  movePlayerByKeys() {
     const getKeyMapCondition = (key) => this.cursor[key].isDown;
     // 当向左运动时
     if (getKeyMapCondition('left')) {
@@ -46,9 +61,14 @@ export class PlayState extends Phaser.State {
     }
   }
   spawnFruits() {
-    const fruits = ['apple', 'banana'];
+    const fruitsMapWithWeight = {
+      banana: 5,
+      apple: 10,
+    };
+
     const randomFruitIndex = this.game.rnd.integerInRange(0, 1);
-    const fruit = this.game.add.sprite(10 + Math.floor(Math.random() * 1000), 0, fruits[randomFruitIndex]);
+    const entity = Object.keys(fruitsMapWithWeight)[randomFruitIndex];
+    const fruit = this.game.add.sprite(10 + Math.floor(Math.random() * 1000), 0, entity);
     this.fruits.add(fruit);
     fruit.scale.setTo(0.02, 0.02);
     fruit.anchor.setTo(0.5, 0.5);
@@ -56,12 +76,13 @@ export class PlayState extends Phaser.State {
     fruit.body.gravity.y = 500;
     fruit.checkWorldBounds = true;
     fruit.outOfBoundsKill = true;
+    fruit.data.points = fruitsMapWithWeight[entity];
   }
   takeFruit(player, fruit) {
     const playerPositon = player.body.position;
     const fruitPosition = fruit.position;
     fruit.kill();
-    this.score += 5;
+    this.score += fruit.data.points;
     this.scoreLabel.text = `score: ${this.score}`;
     // if (fruitPosition.x > playerPositon.x + player.body.halfWidth) {}
   }
