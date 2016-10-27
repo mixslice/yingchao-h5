901d0e662c2e123ac1eab468f240e7043be655f7
郑鑫祺 <candy.zhengxq@gmail.com>
2016-10-27 18:23:37 +0800

    add celebration animation

--- src/states/ResultState.js
+++ src/states/ResultState.js
@@ -2,14 +2,28 @@ export class ResultState extends Phaser.State {
   init() {
     // add init code
     this.successPoints = 200;
+    this.status = Number(this.game.global.score || 0) >= this.successPoints;    
   }
   create() {
-    const status = Number(this.game.global.score || 0) >= this.successPoints;
-    const backgroundMode = status ? 'gameWin' : 'gameFail';
-    const soundMode = status ? 'successSound' : 'failSound';
+    const backgroundMode = this.status ? 'gameWin' : 'gameFail';
+    const soundMode = this.status ? 'successSound' : 'failSound';
     const resultSound = this.game.add.audio(soundMode);
     resultSound.play();
     const background = this.game.add.image(0, 0, backgroundMode);
     background.scale.setTo(this.game.width / background.texture.width, this.game.height / background.texture.height);
+    if (this.status) {
+      const randomDelay = this.game.rnd.integerInRange(200, 300);
+      this.game.time.events.loop(randomDelay, this.animation, this);
+    }
+  }
+  animation() {
+    if (this.celebration) {
+      this.celebration.kill();
+    }
+    const randomPosition = this.game.rnd.integerInRange(-this.game.width, this.game.width);
+    this.celebration = this.game.add.sprite(randomPosition, 0, 'celebration');
+  }
+  goToRank() {
+
   }
 }
